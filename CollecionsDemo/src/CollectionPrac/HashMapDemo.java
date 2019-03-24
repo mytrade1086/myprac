4ddd25fc4b5217bcf2917c7e7828370a950c22bd
mytade1086 <mytrade1086@gmail.com>
2019-03-24 13:15:39 +0530

    hasgmap prg added

---
+++ CollecionsDemo/src/CollectionPrac/HashMapDemo.java
@@ -0,0 +1,26 @@
+package CollectionPrac;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+
+		HashMap<String, Integer> m = new HashMap<>();
+
+		m.put("One", 1);
+		m.put("two", 2);
+		m.put("three", 3);
+		
+		System.out.println(m); // {One=1, two=2, three=3}
+		
+		System.out.println(m.put("three", 4));  //Returns Older value
+		
+		System.out.println(m); // {One=1, two=2, three=4}
+		
+		
+		System.out.println(m.get("u")); //Returns Null when searched non existing key
+
+	}
+
+}
